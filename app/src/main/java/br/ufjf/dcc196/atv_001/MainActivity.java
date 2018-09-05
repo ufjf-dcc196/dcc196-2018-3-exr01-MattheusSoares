fe3324721b7805ed223d5a309ce6a8d27cb74797
Mattheus Soares Santos <tz_santos@hotmail.com>
2018-09-05 20:54:20 -0300

    Atualizando intents

--- app/src/main/java/br/ufjf/dcc196/atv_001/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/atv_001/MainActivity.java
@@ -1,5 +1,6 @@
 package br.ufjf.dcc196.atv_001;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,9 +13,13 @@ public class MainActivity extends AppCompatActivity {
     public static final String SERVIDOR_SIAPE = "";
     public static final String EMAIL_EXTERNO = "";
 
-    public static final int contador_alunos = 0;
-    public static final int contador_servidor = 0;
-    public static final int contador_externo = 0;
+    public static int contador_alunos = 0;
+    public static int contador_servidor = 0;
+    public static int contador_externo = 0;
+
+    public static final int REQUEST_ALUNO = 1;
+    public static final int REQUEST_SERVIDOR = 1;
+    public static final int REQUEST_EXTERNO = 1;
 
     private Button btnCadastrarAluno;
     private Button btnCadastrarServidor;
@@ -32,12 +37,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent_1 = new Intent(MainActivity.this,CadastroAlunoActivity.class);
+                intent_1.putExtra(NOME,contador_alunos);
+                startActivityForResult(intent_1, MainActivity.REQUEST_ALUNO);
+
             }
         });
         btnCadastrarServidor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent_2 = new Intent(MainActivity.this,CadastroServidorActivity.class);
+                intent_2.putExtra(NOME,contador_servidor);
+                startActivityForResult(intent_2, MainActivity.REQUEST_SERVIDOR);
 
             }
         });
@@ -45,8 +55,31 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent_3 = new Intent(MainActivity.this,CadastroExternoActivity.class);
+                intent_3.putExtra(NOME,contador_externo);
+                startActivityForResult(intent_3, MainActivity.REQUEST_EXTERNO);
 
             }
         });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == MainActivity.REQUEST_ALUNO && resultCode == Activity.RESULT_OK && data != null){
+            Bundle bundleResultado = data.getExtras();
+            contador_alunos+=1;
+        }
+        if(requestCode == MainActivity.REQUEST_SERVIDOR && resultCode == Activity.RESULT_OK && data != null){
+            Bundle bundleResultado = data.getExtras();
+            contador_servidor+=1;
+        }
+        if(requestCode == MainActivity.REQUEST_EXTERNO && resultCode == Activity.RESULT_OK && data != null){
+            Bundle bundleResultado = data.getExtras();
+            contador_externo+=1;
+        }
+        //atualiza contador
+
+    }
+
 }
